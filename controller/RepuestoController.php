87cf7a056e99a135ed9f7ad57fb2d4a7647a7cf7
Sergio Rondon <sdrondon@soy.sena.edu.co>
2026-05-29 17:41:26 -0500

    Modificación que permite la comunicación desde React

--- controller/RepuestoController.php
+++ controller/RepuestoController.php
@@ -2,8 +2,13 @@
 // para que React pueda leer este PHP
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Incluimos los archivos necesarios
 include_once '../config/Database.php';
@@ -114,7 +119,4 @@ switch($metodo) {
         }
         break;
 }
-?>
-
-
-        
+?>      
